898acc30f76b2629aa740976103b99f10801d593
Clark Boylan <clark.boylan@gmail.com>
2013-05-11 12:38:24 -0700

    Migrate to pbr.
    Fixes bug 1179007
    
    Change-Id: I6d7a6b7c85361e1568719ad11035158f4f6d9b35

--- setup.py
+++ setup.py
@@ -1,79 +1,23 @@
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-# Copyright 2011 Citrix Systems
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
 
 import setuptools
 
-from quantumclient.openstack.common import setup
-
-Name = 'python-quantumclient'
-Url = "https://launchpad.net/quantum"
-Version = setup.get_post_version('quantumclient')
-License = 'Apache License 2.0'
-Author = 'OpenStack Quantum Project'
-AuthorEmail = 'openstack-dev@lists.launchpad.net'
-Maintainer = ''
-Summary = 'CLI and python client library for OpenStack Quantum'
-ShortDescription = Summary
-Description = Summary
-
-dependency_links = setup.parse_dependency_links()
-tests_require = setup.parse_requirements(['tools/test-requires'])
-
-EagerResources = [
-]
-
-ProjectScripts = [
-]
-
-PackageData = {
-}
-
-
 setuptools.setup(
-    name=Name,
-    version=Version,
-    url=Url,
-    author=Author,
-    author_email=AuthorEmail,
-    description=ShortDescription,
-    long_description=Description,
-    license=License,
-    classifiers=[
-        'Environment :: OpenStack',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-    ],
-    scripts=ProjectScripts,
-    dependency_links=dependency_links,
-    install_requires=setup.parse_requirements(),
-    tests_require=tests_require,
-    cmdclass=setup.get_cmdclass(),
-    include_package_data=False,
-    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    package_data=PackageData,
-    eager_resources=EagerResources,
-    entry_points={
-        'console_scripts': [
-            'quantum = quantumclient.shell:main',
-        ]
-    },
-)
+    setup_requires=['d2to1>=0.2.10,<0.3', 'pbr>=0.5.10,<0.6'],
+    d2to1=True)
