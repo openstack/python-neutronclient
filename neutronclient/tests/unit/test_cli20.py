df93b2716774ec1d044e65bbbd037fdf56313bc7
Qin Zhao <chaochin@gmail.com>
2015-03-06 17:20:06 +0800

    Add InvalidIpForSubnetClient exception
    A new Neutron exception type, InvalidIpForSubnetClient, is added by
    https://review.openstack.org/#/c/121767/ . Need to let Neutron client to
    identify this new exception type.
    
    Change-Id: Ib4f0d7af3807f96463c82d1d2a1e53b56081facc
    Partial-Bug: 1369871

--- neutronclient/tests/unit/test_cli20.py
+++ neutronclient/tests/unit/test_cli20.py
@@ -683,6 +683,7 @@ class CLITestV20ExceptionHandler(CLITestV20Base):
              exceptions.ExternalIpAddressExhaustedClient, 400),
             ('OverQuota', exceptions.OverQuotaClient, 409),
             ('InvalidIpForNetwork', exceptions.InvalidIpForNetworkClient, 400),
+            ('InvalidIpForSubnet', exceptions.InvalidIpForSubnetClient, 400),
         ]
 
         error_msg = 'dummy exception message'
