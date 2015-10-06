21abe4ba34faee99551dbf27134e0482d67876a3
Clark Boylan <clark.boylan@gmail.com>
2015-10-06 14:48:37 -0700

    Let devstack-gate handle the gate hook timeout
    Devstack gate now handles all of the hook timeouts itself so that
    individual jobs do not need to reinvent this wheel. Remove the timeout
    support from the functional test gate hook here as a result.
    
    Change-Id: I3819978b04faea5712c995cbe277ae42870acba9

--- neutronclient/tests/functional/hooks/gate_hook.sh
+++ neutronclient/tests/functional/hooks/gate_hook.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-
 set -ex
 
-source $BASE/new/devstack-gate/functions.sh
-start_timer
-
 VENV=${1:-"functional"}
-
 
 if [ "$VENV" == "functional-adv-svcs" ]
 then
     export DEVSTACK_LOCAL_CONFIG="enable_plugin neutron-vpnaas git://git.openstack.org/openstack/neutron-vpnaas"
 fi
 
-remaining_time
-timeout -s 9 ${REMAINING_TIME}m $BASE/new/devstack-gate/devstack-vm-gate.sh
+$BASE/new/devstack-gate/devstack-vm-gate.sh
