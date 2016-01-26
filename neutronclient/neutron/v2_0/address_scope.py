f9ceed4f4326609f2fbf8263d336dfcae82a5bb6
reedip <reedip.banerjee@nectechnologies.in>
2016-01-26 11:59:31 +0900

    Add 'shared' option to neutron address-scope-update
    With reference to [1], neutron address-scope-update has 'shared'
    option missing. The following patch fixes this issue.
    
    [1]:https://github.com/openstack/neutron/blob/9b830dde9b1f445b0c96d038af66e43e6d4bf0d7/neutron/extensions/address_scope.py
    
    TrivialFix
    
    Change-Id: I520ea471f5ab2681d0890370349433440734ab69

--- neutronclient/neutron/v2_0/address_scope.py
+++ neutronclient/neutron/v2_0/address_scope.py
@@ -15,6 +15,7 @@
 #
 
 from neutronclient._i18n import _
+from neutronclient.common import utils
 from neutronclient.neutron import v2_0 as neutronV20
 
 
@@ -77,8 +78,12 @@ class UpdateAddressScope(neutronV20.UpdateCommand):
     def add_known_arguments(self, parser):
         parser.add_argument('--name',
                             help=_('Updated name of the address scope.'))
+        utils.add_boolean_argument(
+            parser, '--shared',
+            help=_('Set sharing of address scope. '
+                   '(True means shared)'))
 
     def args2body(self, parsed_args):
         body = {}
-        neutronV20.update_dict(parsed_args, body, ['name'])
+        neutronV20.update_dict(parsed_args, body, ['name', 'shared'])
         return {self.resource: body}
