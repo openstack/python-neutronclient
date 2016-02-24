65118c09eb08e760590939e57d3b1a485c567f91
Hirofumi Ichihara <ichihara.hirofumi@lab.ntt.co.jp>
2016-02-24 19:28:28 +0900

    Add wrapper classes for return-request-id-to-caller
    Added wrapper classes which are inherited from base data types tuple, dict
    and str. Each of these wrapper classes contain a 'request_ids' attribute
    which is populated with a 'x-openstack-request-id' received in a header
    from a response body.
    
    This change is required to return 'request_id' from client to log
    request_id mappings of cross projects[1].
    
    [1]: http://specs.openstack.org/openstack/openstack-specs/specs/return-request-id.html
    
    Change-Id: I55fcba61c4efb308f575e95e154aba23e5dd5245
    Implements: blueprint return-request-id-to-caller

--- neutronclient/common/exceptions.py
+++ neutronclient/common/exceptions.py
@@ -60,10 +60,19 @@ class NeutronClientException(NeutronException):
     """
 
     status_code = 0
+    req_ids_msg = _("Neutron server returns request_ids: %s")
+    request_ids = []
 
     def __init__(self, message=None, **kwargs):
+        self.request_ids = kwargs.get('request_ids')
         if 'status_code' in kwargs:
             self.status_code = kwargs['status_code']
+        if self.request_ids:
+            req_ids_msg = self.req_ids_msg % self.request_ids
+            if message:
+                message += '\n' + req_ids_msg
+            else:
+                message = req_ids_msg
         super(NeutronClientException, self).__init__(message, **kwargs)
 
 
