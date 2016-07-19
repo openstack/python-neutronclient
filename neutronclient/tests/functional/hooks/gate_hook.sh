6bc4685f6b50fb8883c67b7f80b12ced6228d27b
Hirofumi Ichihara <ichihara.hirofumi@lab.ntt.co.jp>
2016-07-20 03:09:51 +0900

    Add functional test hook for fwaas command
    This patch adds a hook into functional test so that
    fwaas command test runs.
    
    Change-Id: Ib4ba644b22db0df91a9b0c7011b46ece337198af
    Closes-Bug: #1604510

--- neutronclient/tests/functional/hooks/gate_hook.sh
+++ neutronclient/tests/functional/hooks/gate_hook.sh
@@ -4,6 +4,26 @@ set -ex
 
 VENV=${1:-"functional"}
 
+GATE_DEST=$BASE/new
+NEUTRONCLIENT_PATH=$GATE_DEST/python-neutronclient
+GATE_HOOKS=$NEUTRONCLIENT_PATH/neutronclient/tests/functional/hooks
+DEVSTACK_PATH=$GATE_DEST/devstack
+
+# Inject config from hook into localrc
+function load_rc_hook {
+    local hook="$1"
+    config=$(cat $GATE_HOOKS/$hook)
+    export DEVSTACK_LOCAL_CONFIG+="
+# generated from hook '$hook'
+${config}
+"
+}
+
+if [ "$VENV" == "functional" ]
+then
+    load_rc_hook fwaas
+fi
+
 if [ "$VENV" == "functional-adv-svcs" ]
 then
     export DEVSTACK_LOCAL_CONFIG="enable_plugin neutron-vpnaas git://git.openstack.org/openstack/neutron-vpnaas"
